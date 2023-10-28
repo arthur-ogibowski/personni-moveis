9258255b9bfa2d03852567cdeddb356d4f8aab97
Aguinaldo <guimachado1708@gmail.com>
2023-10-28 17:03:14 -0300

    adicionando tabela de pedidos no dashbord e pedidos adm

--- src/components/user/userNavbar.vue
+++ src/components/user/userNavbar.vue
@@ -3,7 +3,16 @@
     <h1 class="perfil-title"><el-icon style="font-size:25px"><UserFilled /></el-icon>Minha Conta</h1>
     <el-tabs v-model="activeName" @tab-click="handleClick">
     <el-tab-pane label="Geral" name="first"></el-tab-pane>
-    <el-tab-pane label="Pedidos" name="second"></el-tab-pane>
+    <el-tab-pane label="Pedidos" name="second">
+      <el-table :data="pedidos" class="perfil-table" style="width: 100%">
+        <el-table-column prop="orderId" label="#" width="100" />
+        <el-table-column prop="totalPrice" label="Valor Total" />
+        <el-table-column prop="status" label="Status" />
+        <el-table-column>
+          <el-button plain>Detalhes</el-button>
+        </el-table-column>
+      </el-table>
+    </el-tab-pane>
     <el-tab-pane label="Configurações" name="third"></el-tab-pane>
   </el-tabs>
 
@@ -12,8 +21,16 @@
 
 <script>
 import AuthService from '@/store/authService.js';
+import axios from 'axios';
 
 export default {
+  data() {
+    return {
+      pedidos: [],
+      page: 1,
+      pageSize: 10,
+    }
+  },
   methods: {
     handleLogout() {
       this.$confirm('Tem certeza que deseja sair?', 'Confirmação', {
@@ -28,7 +45,17 @@ export default {
         // Usuário cancelou a saída, não faz nada
       });
     }
-  }
+  },
+
+  created() {
+    axios.get('http://localhost:8081/orders')
+      .then(response => {
+        this.pedidos = response.data;
+      })
+      .catch(error => {
+        console.error('Erro ao obter dados da API:', error);
+      });
+  },
 }
 </script>
 
